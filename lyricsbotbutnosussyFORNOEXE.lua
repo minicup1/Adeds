8fdd181e090666abef03fc2ecc9e3c887e130395
minicup1 <97554039+minicup1@users.noreply.github.com>
2024-09-08 18:26:18 +0200

    Update lyricsbotbutnosussyFORNOEXE.lua

--- lyricsbotbutnosussyFORNOEXE.lua
+++ lyricsbotbutnosussyFORNOEXE.lua
@@ -1,15 +1,16 @@
-local songName,plr
+local songName, plr
 local debounce = false
-
 shared.stopped = false
 
 local function sendMessage(text)
 	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(text, "All")
 end
 
-
 game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents:WaitForChild('OnMessageDoneFiltering').OnClientEvent:Connect(function(msgdata)
-	if plr ~= nil and (msgdata.FromSpeaker == plr or msgdata.FromSpeaker == game:GetService('Players').LocalPlayer.Name) then
+	local playerService = game:GetService('Players')
+	local localPlayer = playerService.LocalPlayer
+
+	if plr and (msgdata.FromSpeaker == plr or msgdata.FromSpeaker == localPlayer.Name) then
 		if string.lower(msgdata.Message) == ';stop' then
 			shared.stopped = true
 			debounce = true
@@ -17,52 +18,60 @@ game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents:WaitForChild('O
 			debounce = false
 		end
 	end
-	if debounce or not string.match(msgdata.Message, ';lyrics ') or string.gsub(msgdata.Message, ';lyrics', '') == '' or game:GetService('Players')[msgdata.FromSpeaker] == game:GetService('Players').LocalPlayer then
+
+	if debounce or not string.match(msgdata.Message, ';lyrics ') or string.gsub(msgdata.Message, ';lyrics', '') == '' or playerService[msgdata.FromSpeaker] == localPlayer then
 		return
 	end
+
 	debounce = true
 	local speaker = msgdata.FromSpeaker
-	local msg = string.lower(msgdata.Message):gsub('>lyrics ', ''):gsub('"', ''):gsub(' by ','/')
-	local speakerDisplay = game:GetService('Players')[speaker].DisplayName
-	plr = game:GetService('Players')[speaker].Name
+	local msg = string.lower(msgdata.Message):gsub('>lyrics ', ''):gsub('"', ''):gsub(' by ', '/')
+	local speakerDisplay = playerService[speaker].DisplayName
+	plr = playerService[speaker].Name
 	songName = string.gsub(msg, " ", ""):lower()
+
 	local response
-	local suc,er = pcall(function()
+	local success, errorMsg = pcall(function()
 		response = game.HttpService:RequestAsync({
 			Url = "https://lyrist.vercel.app/api/" .. songName,
 			Method = "GET",
 		})
 	end)
-	if not suc then
+
+	if not success then
 		sendMessage('Unexpected error, please retry')
 		task.wait(3)
 		debounce = false
 		return
 	end
+
 	local lyricsData = game:GetService('HttpService'):JSONDecode(response.Body)
-	local lyricsTable = {}
 	if lyricsData.error and lyricsData.error == "Lyrics Not found" then
-		debounce = true
 		sendMessage('Lyrics were not found')
 		task.wait(3)
 		debounce = false
 		return
 	end
+
+	local lyricsTable = {}
 	for line in string.gmatch(lyricsData.lyrics, "[^\n]+") do
 		table.insert(lyricsTable, line)
 	end
+
 	sendMessage('Fetched lyrics')
 	task.wait(2)
 	sendMessage('Playing song requested by ' .. speakerDisplay .. '. They can stop it by saying ";stop"')
 	task.wait(3)
-	for i, line in ipairs(lyricsTable) do
+
+	for _, line in ipairs(lyricsTable) do
 		if shared.stopped then
 			shared.stopped = false
 			break
 		end
-		sendMessage(`🎙️ | {line}`)
+		sendMessage('🎙️ | ' .. line)
 		task.wait(4.7)
 	end
+
 	task.wait(3)
 	debounce = false
 	sendMessage('Ended. You can request songs again.')
@@ -71,7 +80,7 @@ end)
 task.spawn(function()
 	while task.wait(60) do
 		if not debounce then
-			sendMessage('I am a lyrics bot! Type ;lyrics [SongName]" and I will sing the song for you!')
+			sendMessage('I am a lyrics bot! Type ";lyrics [SongName]" and I will sing the song for you!')
 			task.wait(2)
 			if not debounce then
 				sendMessage('You can also do ";lyrics [SongName by Author]"')
